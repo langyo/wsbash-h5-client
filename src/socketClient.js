4989bc537d3fe4c22cb343ff92a7c7570d6ce28e
langyo <langyo.china@gmail.com>
2019-08-06 04:18:54 +0800

    让此库有了一定的容错能力

--- src/socketClient.js
+++ src/socketClient.js
@@ -3,16 +3,41 @@ const CommandRegister = require('./commandRegister.js');
 
 module.exports = class SocketClient {
   constructor(url) {
-    this.client = new WebSocket(url);
-
     this.commandRegister = new CommandRegister();
-    this.client.onopen = conn => {
-      this.manager = new SocketManager(this.client, this.commandRegister);
-      for(let i of this.openEventList) i(this);
-    };
 
-    this.openEventList = [];
+    const openEvent = e => {
+      this.manager = new SocketManager(this.client, this.commandRegister);
+      for(let i of this.customOpenEventList) i(this);
+    };
+    const errorEvent = e => {
+      for(let i of this.customErrorEventList) i(e);
+      console.log('It will try to reconnect after 1s.');
+      setTimeout(() => {
+        this.client = new WebSocket(url);
+        this.client.onopen = openEvent;
+        this.client.onerror = errorEvent;
+        this.client.onclose = closeEvent;
+      }, 1000);
+    };
+    const closeEvent = e => {
+      console.log('The connection has been closed, and it will try to reconnect after 3s.')
+      setTimeout(() => {
+        this.client = new WebSocket(url);
+        this.client.onopen = openEvent;
+        this.client.onerror = errorEvent;
+        this.client.onclose = closeEvent;
+      }, 1000);
+    }
+
+    this.client = new WebSocket(url);
+    this.client.onopen = openEvent;
+    this.client.onerror = errorEvent;
+    this.client.onclose = closeEvent;
+
+    this.customOpenEventList = [];
     this.onOpen.bind(this);
+    this.customErrorEventList = [];
+    this.onError.bind(this);
 
     this.register.bind(this);
     this.receive.bind(this);
@@ -20,7 +45,12 @@ module.exports = class SocketClient {
 
   onOpen(func) {
     if(!(typeof func === 'function')) throw new Error('You must provide a function!');
-    this.openEventList.push(func);
+    this.customOpenEventList.push(func);
+  }
+
+  onError(func) {
+    if(!(typeof func === 'function')) throw new Error('You must provide a function!');
+    this.customErrorEventList.push(func);
   }
 
   register() { this.commandRegister.register.apply(this.commandRegister, arguments); };
